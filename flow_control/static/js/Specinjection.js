ff4a3da2585b629a5461f6ec0bad19d677d82b35
Diego Millan <diego.millan@edunext.co>
2016-05-27 12:44:55 -0500

    adding first simple test

---
+++ flow_control/static/js/Specinjection.js
@@ -0,0 +1,5 @@
+describe('Injection.js Test', function() {
+    it('vieweblosck.seqContent should be #seq_content', function() {
+        expect(viewblocks.seqContent).toEqual($("#seq_content"));
+    });
+});
